13f0f876bce043b5691ad5823310e892a722692c
oskar <263970@student.pwr.edu.pl>
2022-07-10 16:43:29 +0200

    apply personal formatting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
+#include <spdlog/spdlog.h>
+#include <CLI/CLI.hpp>
 #include <functional>
 #include <iostream>
 #include <optional>
 
-#include <CLI/CLI.hpp>
-#include <spdlog/spdlog.h>
-
-// This file will be generated automatically when you run the CMake configuration step.
-// It creates a namespace called `myproject`.
-// You can modify the source template at `configured_files/config.hpp.in`.
+// This file will be generated automatically when you run the CMake
+// configuration step. It creates a namespace called `myproject`. You can modify
+// the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
-
 // NOLINTNEXTLINE(bugprone-exception-escape)
-int main(int argc, const char **argv)
-{
+int main(int argc, const char** argv) {
   try {
-    CLI::App app{ fmt::format("{} version {}", myproject::cmake::project_name, myproject::cmake::project_version) };
+    CLI::App app{ fmt::format(
+      "{} version {}",
+      myproject::cmake::project_name,
+      myproject::cmake::project_version) };
 
     std::optional<std::string> message;
     app.add_option("-m,--message", message, "A message to print back out");
@@ -37,7 +37,7 @@ int main(int argc, const char **argv)
     } else {
       fmt::print("No Message Provided :(\n");
     }
-  } catch (const std::exception &e) {
+  } catch (const std::exception& e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
   }
 }
